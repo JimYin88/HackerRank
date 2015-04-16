5afbbcf35cd82040fbf8a7b1b19ed03b247f756b
gorillainvestor <JimYin88@gmail.com>
2015-04-16 09:50:17 -0400

    Delete Cut the stick.py

--- Cut the stick.py
+++
@@ -1,9 +0,0 @@
-if __name__ == '__main__':
-    
-    N = int(raw_input())
-    A = map(int, raw_input().split())
-    
-    list1 = A
-    while len(list1) > 0:
-        print len(list1)
-        list1 = [x - min(list1) for x in list1 if x - min(list1) > 0]
