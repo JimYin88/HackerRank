228918c4994b76e67f35a0cbc91f5b56bc366660
gorillainvestor <JimYin88@gmail.com>
2015-04-22 13:39:55 -0400

    Update The Coin Change Problem.py

--- Dynamic-Programming/The Coin Change Problem.py
+++ Dynamic-Programming/The Coin Change Problem.py
@@ -1,23 +1,27 @@
-def coin_change(n, coins_available, coins_so_far):
-    if sum(coins_so_far) == n:
-        yield coins_so_far
-    elif sum(coins_so_far) > n:
-        pass
-    elif coins_available == []:
-        pass
+def coin_change(amount, coins):
+    if amount == 0:
+        return 1
+    elif amount < 0:
+        return 0
+    elif len(coins) == 0:    
+        return 0
     else:
-        for c in coin_change(n, coins_available[:], coins_so_far+[coins_available[0]]):
-            yield c
-        for c in coin_change(n, coins_available[1:], coins_so_far):
-            yield c
-            
-            
-            
+        return coin_change(amount - coins[0], coins) + coin_change(amount, coins[1:])
+
+
+def memoized(func):
+    cache={}
+    def wrapper(*args):
+        if args not in cache:
+            cache[args] = func(*args)
+        return cache[args]
+    return wrapper
+
+coin_change = memoized(coin_change)
+
 if __name__ == '__main__':
     
-    n, m = list(map(int, raw_input().split()))
-    coins = list(map(int, raw_input().split()))
+    N, M = list(map(int, raw_input().split()))
+    coins = tuple(map(int, raw_input().split()))
     
-    solutions = coin_change(n, coins, [])
-    count = sum([1 for x in solutions])
-    print count
+    print coin_change(N, coins)
